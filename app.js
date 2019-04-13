c6f3d6408ca69d8c4d82d785b8d7a76a6df96980
Nick-Hericks <nickhericks@gmail.com>
2019-04-13 12:00:24 +0200

    Create header element

---
+++ app.js
@@ -0,0 +1,26 @@
+const title = React.createElement(
+	'h1',
+	{ id: 'main-title', title: 'This is a title.' },
+	'My first React Element!'
+);
+
+const desc = React.createElement(
+	'p',
+	null,
+	'I just learned how to create a React node and render it to the DOM.'
+);
+
+const header = React.createElement(
+	'header',
+	null,
+	title,
+	desc
+);
+
+
+
+
+ReactDOM.render(
+	header,
+	document.getElementById('root')
+);
