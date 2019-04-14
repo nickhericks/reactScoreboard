8c61cab613f70ea57e19af3db65151361430f5a4
Nick-Hericks <nickhericks@gmail.com>
2019-04-14 09:09:52 +0200

    Add Header component

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 /* eslint-disable no-undef */
-const desc = 'I just learned how to create a React node and render it to the DOM.';
-const myTitleID = 'main-title';
-const name = 'Nick';
 
-const header = (
-	<header>
-		<h1 id={ myTitleID }>{ name }'s first React element!</h1>
-		<p className="main-desc">{ desc }</p>
-	</header>
-);
+function Header() {
+	return (
+		<header>
+			<h1>Scoreboard</h1>
+			<span className='stats'>Players: 1</span>
+		</header>
+
+
+	);
+}
 
 
 ReactDOM.render(
-	header,
+	<Header />,
 	document.getElementById('root')
 );
