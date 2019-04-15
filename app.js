524bbdae52faf55d11bf1af1fa33b71b57f0613b
Nick-Hericks <nickhericks@gmail.com>
2019-04-15 15:24:57 +0200

    Add props for Counter component

--- app.js
+++ app.js
@@ -7,23 +7,25 @@ const Header = (props) => (
 		</header>
 );
 
-const Player = () => {
+const Player = (props) => {
 	return (
 		<div className="player">
 			<span className="player-name">
-				Guil
+				{props.name}
 			</span>
 
-			<Counter />			
+			<Counter 
+				score={ props.score }
+			/>			
 		</div>
 	);
 }
 
-const Counter = () => {
+const Counter = (props) => {
 	return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{ props.score }</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
@@ -33,10 +35,16 @@ const Counter = () => {
 const App = () => {
 	return (
     <div className="scoreboard">
-      <Header title="Scoreboard" totalPlayers={1}/>
+      <Header 
+				title="Scoreboard" 
+				totalPlayers={1}
+			/>
 
 			{/* Players list */}
-      <Player />
+      <Player name="Dan" score={25}/>
+      <Player name="Guil" score={90}/>
+      <Player name="Ashley" score={85}/>
+      <Player name="James" score={80}/>
     </div>
   );
 }
