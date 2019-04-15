4fe1b34674b393e078df1ece3059fcb9b2c00174
Nick-Hericks <nickhericks@gmail.com>
2019-04-15 19:59:27 +0200

    Make Counter class

--- app.js
+++ app.js
@@ -37,22 +37,29 @@ const Player = (props) => {
 				{props.name}
 			</span>
 
-			<Counter 
-				score={ props.score }
-			/>			
+			<Counter />			
 		</div>
 	);
 }
 
+class Counter extends React.Component {
 
-const Counter = (props) => {
-	return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{ props.score }</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
+	constructor() {
+		super()
+		this.state = {
+			score: 0
+		};
+	}
+
+	render() {
+		return (
+			<div className="counter">
+				<button className="counter-action decrement"> - </button>
+				<span className="counter-score">{ this.state.score }</span>
+				<button className="counter-action increment"> + </button>
+			</div>
+		);
+	}
 }
 
 // Main React component to be rendered
@@ -65,7 +72,6 @@ const App = (props) => {
       {props.initialPlayers.map(player => (
         <Player 
 					name={ player.name } 
-					score={ player.score } 
 					key={ player.id.toString() }
 				/>
       ))}
