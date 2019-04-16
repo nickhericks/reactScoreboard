52a044352aa92824278e12d447da5176cc87c90c
Nick-Hericks <nickhericks@gmail.com>
2019-04-16 10:25:44 +0200

    Update App component state

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 /* eslint-disable no-undef */
 
-const players = [
-	{
-    name: "Guil",
-		score: 50,
-		id: 1
-  },
-  {
-    name: "Treasure",
-    score: 85,
-		id: 2
-  },
-  {
-    name: "Ashley",
-    score: 95,
-		id: 3
-  },
-  {
-    name: "James",
-    score: 80,
-		id: 4
-  }
-];
-
 const Header = (props) => (
 		<header>
 			<h1>{ props.title }</h1>
@@ -43,11 +20,12 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
-  state = {
-    score: 0
-  };
 
-  incrementScore = () => {
+	state = {
+		score: 0
+	};
+
+   incrementScore = () => {
     this.setState(prevState => ({
       score: prevState.score + 1
     }));
@@ -59,49 +37,65 @@ class Counter extends React.Component {
     }));
   };
 
+	render() {
+		return (
+			<div className="counter">
+				<button className="counter-action decrement" onClick={ this.decrementScore }> - </button>
+				<span className="counter-score">{ this.state.score }</span>
+				<button className="counter-action increment" onClick={ this.incrementScore }> + </button>
+			</div>
+		);
+	}
+}
+
+// Main React component to be rendered
+class App extends React.Component {
+  state = {
+    players: [
+      {
+        name: "Guil",
+        id: 1
+      },
+      {
+        name: "Treasure",
+        id: 2
+      },
+      {
+        name: "Ashley",
+        id: 3
+      },
+      {
+        name: "James",
+        id: 4
+      }
+    ]
+	};
+	
+	handleRemovePlayer = (id) => {
+		this.setState({
+			players: 
+		});
+	}
+
   render() {
     return (
-      <div className="counter">
-        <button
-          className="counter-action decrement"
-          onClick={this.decrementScore}
-        >
-          {" "}
-          -{" "}
-        </button>
-        <span className="counter-score">{this.state.score}</span>
-        <button
-          className="counter-action increment"
-          onClick={this.incrementScore}
-        >
-          {" "}
-          +{" "}
-        </button>
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          totalPlayers={this.state.players.length}
+        />
+
+        {/* Players list */}
+        {this.state.players.map(player => (
+          <Player name={player.name} key={player.id.toString()} />
+        ))}
       </div>
     );
   }
 }
 
-// Main React component to be rendered
-const App = (props) => {
-	return (
-    <div className="scoreboard">
-      <Header title="Scoreboard" totalPlayers={ props.initialPlayers.length } />
-
-      {/* Players list */}
-      {props.initialPlayers.map(player => (
-        <Player 
-					name={ player.name } 
-					key={ player.id.toString() }
-				/>
-      ))}
-
-    </div>
-  );
-}
-
 // Render React component to the page
 ReactDOM.render(
-	<App initialPlayers={players}/>,
+	<App />,
 	document.getElementById('root')
 );
