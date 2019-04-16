c68b238df2569d6477b720790128d562d20a775e
Nick-Hericks <nickhericks@gmail.com>
2019-04-16 10:05:57 +0200

    Add increment event handler

--- app.js
+++ app.js
@@ -48,12 +48,24 @@ class Counter extends React.Component {
 		score: 0
 	};
 
+	decrementScore = () => {
+		this.setState({
+			score: this.state.score - 1
+		});
+	}
+
+	incrementScore = () => {
+		this.setState({
+			score: this.state.score + 1
+		});
+	}
+
 	render() {
 		return (
 			<div className="counter">
-				<button className="counter-action decrement"> - </button>
+				<button className="counter-action decrement" onClick={ this.decrementScore }> - </button>
 				<span className="counter-score">{ this.state.score }</span>
-				<button className="counter-action increment"> + </button>
+				<button className="counter-action increment" onClick={ this.incrementScore }> + </button>
 			</div>
 		);
 	}
